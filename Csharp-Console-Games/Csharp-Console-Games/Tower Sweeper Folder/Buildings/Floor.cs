337074737155b856f0f5c7250fadc0b1642cb242
Aleksander210 <aleksandar210@abv.bg>
2020-07-24 16:42:54 +0300

    updated new added task

--- Csharp-Console-Games/Csharp-Console-Games/Tower Sweeper Folder/Buildings/Floor.cs
+++ Csharp-Console-Games/Csharp-Console-Games/Tower Sweeper Folder/Buildings/Floor.cs
@@ -13,9 +13,64 @@ namespace Csharp_Console_Games.Tower_Sweeper_Folder.Buildings
 
         //field
         private int floorCount;
+        private char[,] floorField;
         private Dictionary<string, Enemy> currentEnemeisOnFloor;
         //private Dictionary<string, Chest> currentChestsOnFloor;
+        private int[,] floorExit;
+        private int[,] nextFloor;
+       // Player thePlayerOnTheFloor;     //not sure if it has to be like this 
 
-        
+        //construtos
+        public Floor(int floorCount): this()
+        {
+            this.floorCount = floorCount;
+
+        }
+
+        private Floor()
+        {
+            //generate the floor rows and columns
+            this.floorField = new char[RandomNumberGenerator(7,14),RandomNumberGenerator(10,14)];
+
+            //initialise  fields
+            this.currentEnemeisOnFloor = new Dictionary<string, Enemy>();
+
+            //initialise stuff from resources
+            this.sb = new StringBuilder();
+            this.enemyCoordinatesOnFloor = new HashSet<string>();
+            this.chestCoordinatesOnFloor = new HashSet<string>();
+        }
+
+
+        //properties
+        private string FloorField
+        {
+            set
+            {
+                this.sb.Clear();
+                for(int i =0;i<this.floorField.GetLength(0);i++)
+                {
+                    for(int j=0;j<this.floorField.GetLength(1);j++)
+                    {
+                        this.sb.Append(this.floorField[i, j]);
+                    }
+                    this.sb.Append(Environment.NewLine);
+                }
+            }
+
+            get { return this.sb.ToString(); }
+        }
+
+
+
+        //helping methods
+
+        //random number generator
+        private int RandomNumberGenerator(int min, int max)
+        {
+            Random current = new Random();
+            return current.Next(min, max) % max;
+        }
+
     }
 }
