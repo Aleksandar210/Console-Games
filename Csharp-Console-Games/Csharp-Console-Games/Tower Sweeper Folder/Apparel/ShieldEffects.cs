a3e5304ddd7c096904443e67865005c81b428766
Aleksander210 <aleksandar210@abv.bg>
2020-08-18 01:11:46 +0300

    updated new added task

--- Csharp-Console-Games/Csharp-Console-Games/Tower Sweeper Folder/Apparel/ShieldEffects.cs
+++ Csharp-Console-Games/Csharp-Console-Games/Tower Sweeper Folder/Apparel/ShieldEffects.cs
@@ -10,5 +10,45 @@ namespace Csharp_Console_Games.Tower_Sweeper_Folder.Apparel
         private Dictionary<string, Action> currentShieldEffects;
 
         //Actions/effects
+        private Action LionShield = () =>       //stacks 5 roars and on 5 it deals 10*5 damage to enemy
+        {
+
+        };
+
+        private Action SnakeShield = () =>      //when hit it deal half damage to the enemy shield
+         {
+
+         };
+
+        private Action ShieldOfHaskovo = () =>  // increases health if below 50 procent health on hit value
+        {
+
+        };
+
+        private Action PhoenixOfStaraZagora = () =>     //when doestroyed it generates with half health
+        {
+
+        };
+
+        private Action BasicWoodenShield = () =>        //does nothing this is finished :)
+         {
+
+         };
+
+        private Action DualCore2 = () =>        //doubles damage
+         {
+
+         };
+
+        private Action TronShield = ()=>        //throws a disc that goes trough all enemies dealing damage
+        {
+
+        }
+
+        private Action LustShield = () =>       //takes half of the damage value and it absorbs that much hp form enemy
+        {
+
+        };
+
     }
 }
