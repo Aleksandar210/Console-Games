8fb476889ab626009d45a65a98dd40c94c5da800
Aleksandar210 <aleksandar210@abv.bg>
2020-08-20 12:37:58 +0300

    updated new added task

--- Csharp-Console-Games/Csharp-Console-Games/Tower Sweeper Folder/Weapons/Weapon.cs
+++ Csharp-Console-Games/Csharp-Console-Games/Tower Sweeper Folder/Weapons/Weapon.cs
@@ -6,17 +6,26 @@ namespace Csharp_Console_Games.Tower_Sweeper_Folder.Weapons
 {
     public abstract class Weapon
     {
-
+        //resource
+        protected Random rand;
 
         //fields
-        private int damage;
-        private int durability;
-        private string name;
+        protected int damage;
+        protected readonly int durabilityRange;       //this is the constant range
+        protected int currentDurabilityValue;       //this is changable
+        protected string name;
 
         //costructs
-        public Weapon(string name,int damage, int durability)
+        public Weapon(string name,int damage, int durability):this()
         {
+            this.Name = name;
+            this.Damage = damage;
+            this.DurabilityRange = durability;
+        }
 
+        private Weapon()
+        {
+            this.rand = new Random();
         }
 
         //properties
@@ -54,6 +63,38 @@ namespace Csharp_Console_Games.Tower_Sweeper_Folder.Weapons
             get { return this.name; }
         }
 
+        protected int DurabilityRange
+        {
+           private set
+            {
+                if(value<=0 || value>100)
+                {
+                    throw new ArgumentException("Invalid Durability value");
+                }
+            }
+
+
+            get { return this.durabilityRange; }
+        }
+
         //behaviour
+
+        protected void DereaseCurrentDurabilityOnHit()
+        {
+            int decreaseBy = rand.Next(2, 5);
+            this.currentDurabilityValue -= decreaseBy;
+        }
+
+        protected void FixWeaponDurabilityValue(int fixBy)
+        {
+            if(this.currentDurabilityValue+ fixBy<=this.DurabilityRange)
+            {
+                this.currentDurabilityValue += fixBy;
+            }
+            else
+            {
+
+            }
+        }
     }
 }
