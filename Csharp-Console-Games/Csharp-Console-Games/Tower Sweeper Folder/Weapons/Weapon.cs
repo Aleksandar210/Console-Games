1fb703c25311bad83fb69e9e75487ab0368b431f
Aleksandar210 <aleksandar210@abv.bg>
2020-08-20 12:28:11 +0300

    updated new addded task

--- Csharp-Console-Games/Csharp-Console-Games/Tower Sweeper Folder/Weapons/Weapon.cs
+++ Csharp-Console-Games/Csharp-Console-Games/Tower Sweeper Folder/Weapons/Weapon.cs
@@ -6,13 +6,36 @@ namespace Csharp_Console_Games.Tower_Sweeper_Folder.Weapons
 {
     public abstract class Weapon
     {
-        //consts
+
 
         //fields
+        private int damage;
+        private int durability;
+        private string name;
 
         //costructs
+        public Weapon(string name,int damage, int durability)
+        {
+
+        }
 
         //properties
+        protected int Damage
+        {
+            set
+            {
+                if(value<=0 || value >110)
+                {
+                    throw new ArgumentException("Invalid damage value");
+                }
+                else
+                {
+                    this.damage = value;
+                }
+            }
+
+            get { return this.damage; }
+        }
 
         //behaviour
     }
