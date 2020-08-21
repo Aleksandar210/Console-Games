891281f71108d6d4ade7fc8fa226429e06cf88f8
Aleksandar210 <aleksandar210@abv.bg>
2020-08-21 20:01:44 +0300

    updated new added task

--- Csharp-Console-Games/Csharp-Console-Games/Tower Sweeper Folder/Apparel/EmeraldShield.cs
+++ Csharp-Console-Games/Csharp-Console-Games/Tower Sweeper Folder/Apparel/EmeraldShield.cs
@@ -7,11 +7,29 @@ namespace Csharp_Console_Games.Tower_Sweeper_Folder.Apparel
     class EmeraldShield : Shield
     {
         //Emerald Shield -> name
+        //when hit this shields gets only 5% of the attack value which makes him the rarest shield which reminds me to include rare
 
-        public EmeraldShield():base("Emeral Shield",150)
+        //fields
+        
+        private Player player;
+        private Enemy enemy;
+        private bool isPlayerOwner;
+
+        private EmeraldShield() : base("Emeral Shield", 150)
         {
 
         }
+
+        public EmeraldShield(Enemy enemy):this()
+        {
+
+        }
+
+        public EmeraldShield(Player player):this()
+        {
+
+        }
+
         protected override void EffectOnHit()
         {
             throw new NotImplementedException();
